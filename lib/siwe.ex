ac85d29f89555141403a9f515f3ea37930094139
K Rhoda <kelsey.rhoda@spruceid.com>
2021-12-20 17:34:18 -0700

    Expands API and includes testing, needs docs

--- lib/siwe.ex
+++ lib/siwe.ex
@@ -1,8 +1,6 @@
 defmodule Siwe do
   @moduledoc """
   Siwe provides validation and parsing for Sign-In with Ethereum messages and signatures.
-  Exposes the verify!(message, signature) which raises if invalid
-  and returns a Message if valid.
   """
 
   use Rustler, otp_app: :siwe, crate: "siwe_ex"
@@ -11,26 +9,114 @@ defmodule Siwe do
   # of what siwe-rs uses
   defmodule Message do
     defstruct domain: "",
-      address: "",
-      statement: "",
-      uri: "",
-      version: "",
-      chain_id: "",
-      nonce: "",
-      issued_at: "",
-      expiration_time: nil, # or a string datetime
-      not_before: nil, # or a string datetime
-      request_id: nil, # or string
-      resources: []
+              address: "",
+              statement: "",
+              uri: "",
+              version: "",
+              chain_id: "",
+              nonce: "",
+              issued_at: "",
+              expiration_time: nil,
+              # or a string datetime
+              not_before: nil,
+              # or a string datetime
+              request_id: nil,
+              # or string
+              resources: []
   end
 
   # Overwritten by the Rustler NIF, uses siwe-rs to do the heavy lifting.
   @doc false
-  @spec verify(String.t(), String.t()) :: Message.t()
-  defp verify(_msg, _sig) do
+  @spec from_str(String.t()) :: Message.t()
+  defp from_str(_msg) do
     :erlang.nif_error(:nif_not_loaded)
   end
 
+  @spec to_str(Message.t()) :: String.t()
+  defp to_str(_msg) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
+  # Overwritten by the Rustler NIF, uses siwe-rs to do the heavy lifting.
+  @doc false
+  @spec validate_sig(Message.t(), String.t()) :: boolean()
+  def validate_sig(_msg, _sig) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
+  # Overwritten by the Rustler NIF, uses siwe-rs to do the heavy lifting.
+  @doc false
+  @spec validate_time(Message.t()) :: boolean()
+  def validate_time(_msg) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
+  # Overwritten by the Rustler NIF, uses siwe-rs to do the heavy lifting.
+  # Optimized form of validate_sig(m) && validate_time(m)
+  @doc false
+  @spec validate(Message.t(), String.t()) :: boolean()
+  def validate(_msg, _sig) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
+  # Overwritten by the Rustler NIF, uses siwe-rs to do the heavy lifting.
+  # Optimized form of validate_sig(from_str(s))
+  @doc false
+  @spec parse_if_valid_sig(String.t(), String.t()) :: Message.t()
+  defp parse_if_valid_sig(_msg, _sig) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
+  # Overwritten by the Rustler NIF, uses siwe-rs to do the heavy lifting.
+  # Optimized form of validate_time(from_str(s))
+  @doc false
+  @spec parse_if_valid_time(String.t()) :: Message.t()
+  defp parse_if_valid_time(_msg) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
+  # Overwritten by the Rustler NIF, uses siwe-rs to do the heavy lifting.
+  # Optimized form of validate(from_str(s)) && from_str(s)
+  @doc false
+  @spec parse_if_valid(String.t(), String.t()) :: Message.t()
+  defp parse_if_valid(_msg, _sig) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+
+  @doc """
+    Parses a SIWE message string into Siwe.Message struct if conforming to standards.
+    Will parse messages without checking for validity.
+  """
+  @spec from_str!(String.t()) :: Message
+  def from_str!(msg) do
+    from_str(msg)
+  end
+
+  @doc """
+    Formats parsed SIWE message into string matching signing material
+  """
+  @spec to_str!(String.t()) :: Message.t()
+  def to_str!(msg) do
+    to_str(msg)
+  end
+
+  @doc """
+    Parses a SIWE message string into a Siwe Message Struct if the given signature matches
+    the message string.
+  """
+  @spec parse_if_valid_sig!(String.t(), String.t()) :: Message.t()
+  def parse_if_valid_sig!(msg, sig) do
+    parse_if_valid_sig(msg, sig)
+  end
+
+  @doc """
+    Parses a SIWE message string into a Siwe Message Struct if the current time is valid in
+    terms of the message string.
+  """
+  @spec parse_if_valid_time!(String.t()) :: Message.t()
+  def parse_if_valid_time!(msg) do
+    parse_if_valid_time(msg)
+  end
 
   @doc """
    Tests that a message and signature pair correspond and that the current
@@ -41,7 +127,7 @@ defmodule Siwe do
    Returns a Message structure based on the passed message
 
    ## Examples
-    iex> Siwe.verify!(Enum.join(["login.xyz wants you to sign in with your Ethereum account:",
+    iex> Siwe.parse_if_valid!(Enum.join(["login.xyz wants you to sign in with your Ethereum account:",
     ...> "0xfA151B5453CE69ABf60f0dbdE71F6C9C5868800E",
     ...> "",
     ...> "Sign-In With Ethereum Example Statement",
@@ -55,8 +141,8 @@ defmodule Siwe do
     ...> "0x8d1327a1abbdf172875e5be41706c50fc3bede8af363b67aefbb543d6d082fb76a22057d7cb6d668ceba883f7d70ab7f1dc015b76b51d226af9d610fa20360ad1c")
     %{ __struct__: Siwe, address: "0xfA151B5453CE69ABf60f0dbdE71F6C9C5868800E", chain_id: "1", domain: "login.xyz", expiration_time: nil, issued_at: "2021-12-17T00:38:39.834Z", nonce: "ToTaLLyRanDOM", not_before: nil, request_id: nil, resources: [], statement: "Sign-In With Ethereum Example Statement", uri: "https://login.xyz", version: "1" }
   """
-  @spec verify!(String.t(), String.t()) :: Message.t()
-  def verify!(message, signature) do
-    verify(message, signature)
+  @spec parse_if_valid!(String.t(), String.t()) :: Message.t()
+  def parse_if_valid!(message, signature) do
+    parse_if_valid(message, signature)
   end
 end
--- native/siwe_ex/src/lib.rs
+++ native/siwe_ex/src/lib.rs
@@ -1,6 +1,8 @@
 use ethers_core::{types::H160, utils::to_checksum};
 use hex::FromHex;
-use rustler::NifStruct;
+use http::uri::Authority;
+use iri_string::types::UriString;
+use rustler::{Error, NifResult, NifStruct};
 use siwe::eip4361::{Message, Version};
 use std::str::FromStr;
 
@@ -21,36 +23,160 @@ pub struct Parsed {
     pub resources: Vec<String>,
 }
 
+impl Parsed {
+    pub fn to_eip4361_message(&self) -> NifResult<Message> {
+        let mut next_resources: Vec<UriString> = Vec::new();
+        for resource in &self.resources {
+            let x = UriString::from_str(resource).map_err(|_e| Error::BadArg)?;
+            next_resources.push(x);
+        }
+
+        Ok(Message {
+            domain: Authority::from_str(&self.domain).map_err(|_e| Error::BadArg)?,
+            address: <[u8; 20]>::from_hex(self.address.chars().skip(2).collect::<String>())
+                .map_err(|_e| Error::BadArg)?,
+            statement: self.statement.to_string(),
+            uri: UriString::from_str(&self.uri).map_err(|_e| Error::BadArg)?,
+            version: Version::from_str(&self.version).map_err(|_e| Error::BadArg)?,
+            chain_id: self.chain_id.to_string(),
+            nonce: self.nonce.to_string(),
+            issued_at: self.issued_at.to_string(),
+            expiration_time: self.expiration_time.clone(),
+            not_before: self.not_before.clone(),
+            request_id: self.request_id.clone(),
+            resources: next_resources,
+        })
+    }
+}
+
 fn version_string(v: Version) -> String {
     match v {
         Version::V1 => "1".to_string(),
     }
 }
 
+fn message_to_parsed(m: Message) -> Parsed {
+    Parsed {
+        domain: m.domain.to_string(),
+        address: to_checksum(&H160(m.address.into()), None),
+        statement: m.statement,
+        uri: m.uri.to_string(),
+        version: version_string(m.version),
+        chain_id: m.chain_id,
+        nonce: m.nonce,
+        issued_at: m.issued_at,
+        expiration_time: m.expiration_time,
+        not_before: m.not_before,
+        request_id: m.request_id,
+        resources: m.resources.into_iter().map(|s| s.to_string()).collect(),
+    }
+}
+
 #[rustler::nif]
-fn verify(message: String, sig: String) -> Parsed {
-    // TODO: Replace Unwraps w/ Result.
-    let s = <[u8; 65]>::from_hex(sig.chars().skip(2).collect::<String>()).unwrap();
-    match Message::from_str(&message) {
-        Err(e) => panic!("{}", e),
-        Ok(m) => match m.verify_eip191(s) {
-            Ok(_) => Parsed {
-                domain: m.domain.to_string(),
-                address: to_checksum(&H160(m.address.into()), None),
-                statement: m.statement,
-                uri: m.uri.to_string(),
-                version: version_string(m.version),
-                chain_id: m.chain_id,
-                nonce: m.nonce,
-                issued_at: m.issued_at,
-                expiration_time: m.expiration_time,
-                not_before: m.not_before,
-                request_id: m.request_id,
-                resources: m.resources.into_iter().map(|s| s.to_string()).collect(),
+fn from_str(message: String) -> NifResult<Parsed> {
+    Ok(message_to_parsed(
+        Message::from_str(&message).map_err(|_e| Error::BadArg)?,
+    ))
+}
+
+#[rustler::nif]
+fn to_str(message: Parsed) -> NifResult<String> {
+    Ok(message
+        .to_eip4361_message()
+        .map_err(|_e| Error::BadArg)?
+        .to_string())
+}
+
+#[rustler::nif]
+fn validate_sig(message: Parsed, sig: String) -> bool {
+    match message.to_eip4361_message() {
+        Ok(m) => match <[u8; 65]>::from_hex(sig.chars().skip(2).collect::<String>()) {
+            Ok(s) => match m.verify_eip191(&s) {
+                Ok(_) => true,
+                Err(_) => false,
             },
-            Err(e) => panic!("{}", e),
+            Err(_) => false,
+        },
+        Err(_) => false,
+    }
+}
+
+#[rustler::nif]
+fn validate_time(message: Parsed) -> bool {
+    match message.to_eip4361_message() {
+        Ok(m) => m.valid_now(),
+        Err(_) => false,
+    }
+}
+
+#[rustler::nif]
+fn validate(message: Parsed, sig: String) -> bool {
+    match message.to_eip4361_message() {
+        Ok(m) => match <[u8; 65]>::from_hex(sig.chars().skip(2).collect::<String>()) {
+            Ok(s) => match m.verify_eip191(&s) {
+                Ok(_) => m.valid_now(),
+                Err(_) => false,
+            },
+            Err(_) => false,
+        },
+        Err(_) => false,
+    }
+}
+
+#[rustler::nif]
+fn parse_if_valid_time(message: String) -> NifResult<Parsed> {
+    match Message::from_str(&message) {
+        Err(_) => Err(Error::BadArg),
+        Ok(m) => {
+            if m.valid_now() {
+                Ok(message_to_parsed(m))
+            } else {
+                Err(Error::BadArg)
+            }
+        }
+    }
+}
+
+#[rustler::nif]
+fn parse_if_valid_sig(message: String, sig: String) -> NifResult<Parsed> {
+    let s = <[u8; 65]>::from_hex(sig.chars().skip(2).collect::<String>())
+        .map_err(|_e| Error::BadArg)?;
+    let m = Message::from_str(&message).map_err(|_e| Error::BadArg)?;
+    m.verify_eip191(&s).map_err(|_e| Error::BadArg)?;
+
+    Ok(message_to_parsed(m))
+}
+
+#[rustler::nif]
+fn parse_if_valid(message: String, sig: String) -> NifResult<Parsed> {
+    let s = <[u8; 65]>::from_hex(sig.chars().skip(2).collect::<String>())
+        .map_err(|_e| Error::BadArg)?;
+    match Message::from_str(&message) {
+        Err(_) => Err(Error::BadArg),
+        Ok(m) => match m.verify_eip191(&s) {
+            Ok(_) => {
+                if m.valid_now() {
+                    Ok(message_to_parsed(m))
+                } else {
+                    Err(Error::BadArg)
+                }
+            }
+
+            Err(_) => Err(Error::BadArg),
         },
     }
 }
 
-rustler::init!("Elixir.Siwe", [verify]);
+rustler::init!(
+    "Elixir.Siwe",
+    [
+        from_str,
+        to_str,
+        validate_sig,
+        validate_time,
+        validate,
+        parse_if_valid_sig,
+        parse_if_valid_time,
+        parse_if_valid
+    ]
+);
